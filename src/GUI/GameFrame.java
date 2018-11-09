0acf17be15f8f1019f0ce7c52b9bb62927602cc9
pmate955 <pmate955@gmail.com>
2018-11-09 09:55:54 +0100

    wip

--- src/GUI/GameFrame.java
+++ src/GUI/GameFrame.java
@@ -17,7 +17,7 @@ public class GameFrame extends JFrame {
 	private int width, height;
 	
 	public GameFrame(){
-		this.setTitle("Jatekunk");
+		this.setTitle("Jatekunk v1");
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setSize(400, 400);
 		this.setLayout(new BorderLayout());
